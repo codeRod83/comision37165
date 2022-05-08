23b11a0792c04671b298afebf972eecdcb812e98
Rodrigo Floreschapa <rodrigo.floreschapa@gmail.com>
2022-05-08 11:56:25 -0500

    Cambios en acomodo de componentes

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -13,34 +13,35 @@ const ItemCount = ({ countInicial, stock, producto }) => {
     }
     
 
-    return (    <div>
-        <div className="container mt-3 text-center">
-            <button
-                className="btn btn-info"
-                onClick={() => cambiaCount(-1)}
-                disabled={count <= 1}
-            >
-                -
-            </button>
-            <button className="btn btn-light">{count}</button>
-            <button
-                className="btn btn-info"
-                onClick={() => cambiaCount(1)}
-                disabled={count >= stock}
-            >
-                +
-            </button>
+    return (
+        <div>
+            <div className="container mt-3 text-center">
+                <button
+                    className="btn btn-info"
+                    onClick={() => cambiaCount(-1)}
+                    disabled={count <= 1}
+                >
+                    -
+                </button>
+                <button className="btn btn-light">{count}</button>
+                <button
+                    className="btn btn-info"
+                    onClick={() => cambiaCount(1)}
+                    disabled={count >= stock}
+                >
+                    +
+                </button>
+            </div>
+            <div className="container mt-3 text-center">
+                <button
+                    className="btn btn-primary"
+                    onClick={() => onAdd(count)}
+                >
+                    Agregar al Carrito
+                </button>
+            </div>
         </div>
-        <div className="container mt-3 text-center">
-            <button
-                className="btn btn-primary"
-                onClick={() => onAdd(count)}
-            >
-                Agregar al Carrito
-            </button>
-        </div>
-    </div>
-        )
-    }
+    )
+}
 
 export default ItemCount;
