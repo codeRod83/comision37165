ae52ec075338b673950788da6eb9440ebddc39e5
Rodrigo Floreschapa <rodrigo.floreschapa@gmail.com>
2022-06-07 00:58:33 -0500

    Se reacomodo styles.scss fuera de carpeta sass, al igual que los partials, ya que generaban conflictos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '../src/sass/styles.scss'
+import './styles.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
 import { getFirestoreApp } from './firebase/config'
